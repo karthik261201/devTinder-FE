669cff4e80fafaf5ad9b26e39a393ac4e6f01f17
karthik261201 <karthiknarayanan043@gmail.com>
2025-03-30 15:00:03 +0530

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-function App() {
-  return (
-    <>
-      
-    </>
-  );
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./Body";
+import Profile from "./Profile";
+import Login from "./Login";
+
+const App = () => {
+	return (
+	<>
+		<BrowserRouter basename="/">
+			<Routes>
+				<Route path="/" element={<Body />}>
+					<Route path="/profile" element={<Profile />} />
+					<Route path="/login" element={<Login />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	</>
+	);
 }
 
 export default App;
